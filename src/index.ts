84590b92df6eaadec72ffc8da39b0ac631a9b197
Eric Rajaonarison <eric.raj12@gmail.com>
2022-01-22 22:55:21 +0300

    api setup

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer, gql } from "apollo-server";
+import { books } from "./utils/data";
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
